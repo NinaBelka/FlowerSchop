b3d5eb41ca6180c114dbe921bf5823e6ec0b4c05
Nina <mysha76@mail.ru>
2021-02-03 22:43:24 +0300

    Сверстаны 2 блока. Добавлены 2 слайдера

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const flowersSlider = new Swiper('.flowers-slider', {
+  loop: true,
+  slidesPerView: 6,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
